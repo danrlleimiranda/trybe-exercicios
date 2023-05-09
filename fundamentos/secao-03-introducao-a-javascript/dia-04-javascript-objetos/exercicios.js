4e4e867f943808fc3850c9a2083fa8b7da54fa54
Danrllei Miranda <danmiranda14@gmail.com>
2023-05-09 10:30:00 -0300

    nova pasta e arquivo

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicios.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicios.js
@@ -65,3 +65,59 @@ let leitor = {
 //   3 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem {quantidade} livros favoritos”, em que “{quantidade}” corresponde à quantidade de livros favoritos e é um número gerado automaticamente por seu código.
 
 console.log('Julia tem', leitor.livrosFavoritos.length, 'livros favoritos');
+
+let order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        },
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        },
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      },
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  function customerInfo(order) {
+    // Adicione abaixo as informações necessárias.
+    // console.log('Ola', order.order.delivery.deliveryPerson + ', entrega para: ' + order.name +', Telefone: ' + order.phoneNumber + ', R. ' + order.address.street + ', Nº: ' + order.address.number + ', AP: ' + order.address.apartment)
+  }
+  
+  customerInfo(order);
+  
+  function orderModifier(order) {
+    // Adicione abaixo as informações necessárias.
+    let newBuyer = order.name = 'Luiz Silva';
+    let newTotal = order.payment.total = '50';
+    let pizzas = Object.keys(order.order.pizza);
+    let drinks = order.order.drinks.coke.type;
+  
+    return console.log('Olá ' + newBuyer + ', o total do seu pedido de ' + pizzas[0] + ', ' + pizzas[1] + ' e ' + drinks + ' é R$ ' + newTotal + ',00.');
+  }
+  
+
+  console.log(orderModifier(order));
