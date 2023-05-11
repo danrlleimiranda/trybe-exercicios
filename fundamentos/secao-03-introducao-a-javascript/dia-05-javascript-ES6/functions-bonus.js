5f9ddcb6bc5ea45342aef554721e774eaaeba6b9
Danrllei Miranda <danmiranda14@gmail.com>
2023-05-11 10:45:55 -0300

    exercicio bonus

--- fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-ES6/functions-bonus.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-ES6/functions-bonus.js
@@ -1,6 +1,6 @@
 function substituaX(nome) {
-const frase = 'Tryber x aqui!'
-let fraseArray = frase.split(' ');
+    const frase = 'Tryber x aqui!'
+    let fraseArray = frase.split(' ');
     for (let index = 0; index < fraseArray.length; index += 1) {
         if (fraseArray[index] === 'x') {
             fraseArray[index] = nome
@@ -8,4 +8,17 @@ let fraseArray = frase.split(' ');
     }
     return fraseArray.join(' ')
 }
-console.log(substituaX('Bebeto'))
+// console.log(substituaX('Bebeto'))
+    
+function minhasSkills(frase) {
+    const skills = ['JavaScript', 'HTML', 'CSS']
+    let result = `${frase}
+    Minhas três principais habilidades são:`
+    for (let index = 0; index < skills.length; index += 1) {
+        result = `${result}
+        - ${skills[index]}`
+        
+    }
+    return result;
+}
+console.log(minhasSkills(substituaX('Bebeto')))
