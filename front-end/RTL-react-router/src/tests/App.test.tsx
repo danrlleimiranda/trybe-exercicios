12b70ed2327b84fe2d37352050ffdc46c8708a06
Danrllei Miranda <danmiranda14@gmail.com>
2023-10-24 08:35:34 -0300

    novos exercicios

---
+++ front-end/RTL-react-router/src/tests/App.test.tsx
@@ -0,0 +1,16 @@
+// src/App.test.tsx
+
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event'
+import App from '../App';
+
+ it('Navega para página About', async () => {
+        render(<App />, { wrapper: BrowserRouter });
+        expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
+      
+        const aboutLink = screen.getByRole('link', { name: /Sobre/i});
+        await userEvent.click(aboutLink);
+        expect(screen.getByText(/Você está na página Sobre/i)).toBeInTheDocument();
+      })
+      
